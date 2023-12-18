78aec4dc49689f6daa9a122d054f8c65c8f56de2
Bart Nagel <bart@tremby.net>
2023-12-17 22:24:06 -0800

    Ensure directory exists

--- get-hours.mjs
+++ get-hours.mjs
@@ -1,5 +1,7 @@
-import { writeFile } from "node:fs/promises";
+import { mkdir, writeFile } from "node:fs/promises";
 import { getHours } from "./lib/hours.mjs";
 
 const data = await getHours();
+
+await mkdir("./generated", { recursive: true });
 await writeFile("./generated/hours.json", JSON.stringify(data));
